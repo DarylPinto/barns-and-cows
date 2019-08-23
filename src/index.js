31b5f98d04c115665e69a4f2a05f4c111815f3c9
Daryl Pinto <daryl.j.pinto@gmail.com>
2019-08-23 16:25:15 -0400

    Move global styles

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Welcome from "screens/Welcome";
 import Play from "screens/Play";
-import "./index.scss";
+import "assets/styles/global.scss";
 
 document.title = "Barns and Cows";
 
