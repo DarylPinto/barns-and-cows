26c81f9c2aaa50b9d19870f69b0155e1cf5936ca
Daryl Pinto <daryl.j.pinto@gmail.com>
2019-09-05 12:00:43 -0400

    Add dummy buttons to Welcome screen

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -8,7 +8,23 @@ const Welcome = () => {
 			<h1>
 				Barns<span>and</span>Cows
 			</h1>
-			<Button to="/play" tier="primary">Play</Button>
+			<Button to="/play" tier="primary">
+				Play
+			</Button>
+			<Button to="/endless" tier="secondary">
+				Endless
+			</Button>
+			<Button to="/daily" tier="secondary">
+				Daily Levels
+			</Button>
+			<div className="small">
+				<Button to="/settings" tier="secondary">
+					<i className="fas fa-cog" />
+				</Button>
+				<Button to="/tutorial" tier="secondary">
+					<i className="fas fa-question" />
+				</Button>
+			</div>
 		</div>
 	);
 };
